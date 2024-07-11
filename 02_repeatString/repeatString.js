ad105f7c133857292efbc27cbc022c8a07953398
XAJX <88792057+XAJX179@users.noreply.github.com>
2024-07-12 00:49:10 +0530

    pass repeat strings

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string, numberOfRepeat) {
+    
+    if(numberOfRepeat < 0){return 'ERROR'}
 
+    let repeatString ='';
+    for(let i =0;i<numberOfRepeat;i++){
+        repeatString += string;
+    }
+    
+    return repeatString;
 };
 
 // Do not edit below this line
