63d3026d94711b3de8b59210a9797b7c79f3e626
XAJX <88792057+XAJX179@users.noreply.github.com>
2024-07-12 17:10:46 +0530

    pass remove from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(Arr,...args) {
+for(arg of args)
+   {
+    while(Arr.indexOf(arg) > -1){
+        let index = Arr.indexOf(arg);
+        Arr.splice(index,1);
+    }
+   }
+return Arr; 
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
