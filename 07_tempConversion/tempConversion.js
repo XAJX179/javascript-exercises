499119719659bfd37da32484ddf59d7695a29c38
XAJX <88792057+XAJX179@users.noreply.github.com>
2024-07-13 21:27:21 +0530

    pass temperature conversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (Fahrenheit) {
+  let result = (Fahrenheit - 32) * (5 / 9);
+  result = Math.round(result * 10) / 10;
+  return result;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (Celsius) {
+  let result = Celsius * (9 / 5) + 32;
+  result = Math.round(result * 10) / 10;
+  return result;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
