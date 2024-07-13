2849d8a26583d31b56b31e1997e1399cbc49f13a
XAJX <88792057+XAJX179@users.noreply.github.com>
2024-07-13 19:11:43 +0530

    pass sum of all between

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(number1,number2) {
+    if(typeof number1 !== 'number' || typeof number2 !== 'number'){
+        return 'ERROR'
+    }
+    let sum = 0;
+    if(number1 >= number2 && number2 >= 0){
+        for(let i = number2;i <= number1;i++){
+            sum += i
+        }
+        return sum
+    }
+    else if(number2 >= number1 && number1 >= 0){
+        for(let i = number1;i <= number2;i++){
+            sum += i
+        }
+        return sum
+    }
+    else{
+        return 'ERROR';
+    }
 };
-
 // Do not edit below this line
 module.exports = sumAll;
