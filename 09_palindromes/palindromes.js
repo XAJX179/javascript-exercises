8b072c1b38599d8885dd4311b85a85b5befc283b
XAJX <88792057+XAJX179@users.noreply.github.com>
2024-08-03 01:40:03 +0530

    pass : palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  filtered = str
+    .toLowerCase()
+    .replace(/[^a-z0-9]/g, "")
+    .split("");
 
+  let reversed = filtered.slice();
+  reversed = reversed.reverse();
+
+  console.log(`filtered : ${filtered}`);
+  console.log(`reversed : ${reversed}`);
+  if (filtered.join("") === reversed.join("")) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
