f4d842440a83ab5b0887f4a5906a2aea49d379b6
XAJX <88792057+XAJX179@users.noreply.github.com>
2024-08-03 16:32:45 +0530

    pass : fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+  num = +num;
+  if (num === 0) {
+    return 0;
+  } else if (num === 1) {
+    return 1;
+  } else if (num < 0) {
+    return "OOPS";
+  }
+  let fibonacciNum;
+  let prev1 = 0;
+  let prev2 = 1;
+  for (let i = 2; i <= num; i++) {
+    fibonacciNum = prev1 + prev2;
+    prev1 = prev2;
+    prev2 = fibonacciNum;
+  }
+  return fibonacciNum;
 };
-
 // Do not edit below this line
 module.exports = fibonacci;
