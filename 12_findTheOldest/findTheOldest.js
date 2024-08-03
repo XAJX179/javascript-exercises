d700eb0656179ee1ff4c1d7b01a5b5663482d927
XAJX <88792057+XAJX179@users.noreply.github.com>
2024-08-03 19:38:50 +0530

    pass : findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,29 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  // [{2},{2},{3}]
+  // [{3}]
+  let mapped = people
+    .map((item) => {
+      if (typeof item.yearOfDeath == "undefined") {
+        item.yearOfDeath = new Date().getFullYear();
+      }
+      item.age = item.yearOfDeath - item.yearOfBirth;
+      return { name: item.name, age: item.age };
+    })
+    .reduce(
+      (obj, item) => {
+        console.log(item);
+        console.log(obj);
+        if (obj.age < item.age) {
+          obj.name = item.name;
+          obj.age = item.age;
+          return obj;
+        }
+        return obj;
+      },
+      { name: undefined, age: 0 }
+    );
 
+  return mapped;
 };
-
 // Do not edit below this line
 module.exports = findTheOldest;
